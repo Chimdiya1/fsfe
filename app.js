e6a338fb865cad9163ab889c750ca7498c134e84
chimdia <fortunechimdiya@gmail.com>
2020-10-18 20:44:09 +0100

    demo

--- app.js
+++ app.js
@@ -6,4 +6,10 @@ app.get('/', (req,res) => {
 	res.send('mmasi my baby girl, kee way?');
 })
 
+app.get('demo', (req, res) => {
+	res.set('X-full-stack', 'baby boy 4l')
+	res.status(418);
+	res.send('i prefer monster energy drink');
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
